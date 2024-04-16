ec821f57e1e4f392b572f72ae52aa049fe6e3a45
Alfero Chingono <2288092+achingono@users.noreply.github.com>
2024-04-16 13:45:21 -0400

    Add project files and code for XDT transformation

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # poc-xdt-transform
 POC: Web.config Transformation Using XDT
+
+Reference: [XmlTransformTest.cs](https://github.com/dotnet/xdt/blob/main/tests/Microsoft.Web.XmlTransform.Tests/XmlTransformTest.cs)
---
+++ src/Program.cs
@@ -0,0 +1,34 @@
+﻿string src = "web.config";
+string transformFile = "web.release.config";
+string destFile = "web.transformed.config";
+
+//execute
+var document = new Microsoft.Web.XmlTransform.XmlTransformableDocument();
+document.PreserveWhitespace = true;
+document.Load(src);
+
+var transform = new Microsoft.Web.XmlTransform.XmlTransformation(transformFile);
+
+bool succeed = transform.Apply(document);
+
+if (!succeed)
+    throw new Exception("Transformation failed");
+
+document.Save(destFile);
+
+
+//sanity verify the content is right, (xml was transformed)
+string content = File.ReadAllText(destFile);
+var test = content.Contains("debug=\"true\"", StringComparison.OrdinalIgnoreCase);
+
+if (test)
+    throw new Exception("Transformation failed");
+
+var lines = new List<string>(File.ReadLines(destFile));
+//sanity verify the line format is not lost (otherwise we will have only one long line)
+if (lines.Count <= 10)
+    throw new Exception("Transformation failed");
+
+//be nice 
+transform.Dispose();
+document.Dispose();
